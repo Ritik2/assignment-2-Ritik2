b65c0aa8c79ff58efac2ed6b42174862ffa34644
Ritik2 <54735315+Ritik2@users.noreply.github.com>
2020-03-28 21:38:54 +0530

    Update Node.java

--- src/problem3/node/Node.java
+++ src/problem3/node/Node.java
@@ -1,10 +1,12 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:13 PM
- */
 package problem3.node;
 
 public class Node {
+        private int roll;
+
+        public Node(int roll){
+            this.roll=roll;
+        }
+        public int getRoll() {
+            return roll;
+        }
 }
